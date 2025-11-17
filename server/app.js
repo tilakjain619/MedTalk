09fbf2c67df6d2e744133b97b8e1039e5f402c67
tilakjain619 <frontendchamp123@gmail.com>
2025-11-17 19:27:21 +0530

    implement custom CORS policy for enhanced security

--- server/app.js
+++ server/app.js
@@ -5,8 +5,21 @@ import aiRoutes from "./routes/ai.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
 app.use(express.json());
+
+const allowedOrigins = ['http://localhost:5173']
+app.use(cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  }));
 
 app.use("/api", aiRoutes);
 
